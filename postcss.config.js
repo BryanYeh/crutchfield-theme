52f868168f9b99c5e1d0fdfc9a183344363640cd
BryanYeh <bbryanyeh@gmail.com>
2020-10-25 20:25:01 -0700

    added header (desktop nav,notification bar)

--- postcss.config.js
+++ postcss.config.js
@@ -1,31 +1,32 @@
 module.exports = {
-    plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-        require('@fullhuman/postcss-purgecss')({
-            content: [
-                './src/layout/*.liquid',
-                './src/templates/*.liquid',
-                './src/sections/*.liquid',
-                './src/snippets/*.liquid',
-                './src/config/settings_schema.json'
-            ],
-            defaultExtractor: content => content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
-        }),
-        process.env.NODE_ENV === 'production' ? require('cssnano')({ preset: 'default' }) : null,
-        require('copyfiles')(
-            ["./src/**/*.js",
-                "./src/**/*.liquid",
-                "./src/**/*.json",
-                "./dist"
-            ],
-            {
-                up : 1,
-                verbose : false
-            },
-            (err) => {
-                if(err) console.log(err);
-            }
-        )
-    ]
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './src/layout/*.liquid',
+        './src/templates/*.liquid',
+        './src/sections/*.liquid',
+        './src/snippets/*.liquid',
+        './src/config/settings_schema.json',
+        './src/assets/application.js'
+      ],
+      defaultExtractor: content => content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+    }),
+    process.env.NODE_ENV === 'production' ? require('cssnano')({ preset: 'default' }) : null,
+    require('copyfiles')(
+      ["./src/**/*.js",
+        "./src/**/*.liquid",
+        "./src/**/*.json",
+        "./dist"
+      ],
+      {
+        up: 1,
+        verbose: false
+      },
+      (err) => {
+        if (err) console.log(err);
+      }
+    )
+  ]
 }
--- src/assets/application.js
+++ src/assets/application.js
@@ -1,1 +1,40 @@
-// Put your application javascript here
+var ready = (callback) => {
+  if (document.readyState != "loading") callback();
+  else document.addEventListener("DOMContentLoaded", callback);
+}
+
+ready(() => {
+  function closeDropdown() {
+    document.querySelectorAll('.subnav').forEach(subnavLink => {
+      subnavLink.classList.remove('border-red-600')
+      subnavLink.classList.remove('border-solid')
+      subnavLink.classList.remove('border-b-4')
+      subnavLink.querySelector('span>i').classList.remove('transform')
+      subnavLink.querySelector('span>i').classList.remove('rotate-180')
+      subnavLink.querySelector('div').classList.add('hidden')
+    })
+  }
+
+  document.querySelectorAll('.subnav').forEach(subnavLink => {
+    subnavLink.addEventListener('click', (e) => {
+      let shouldOpen = e.target.parentNode.classList.contains('subnav') && e.target.parentNode.querySelector('div').classList.contains('hidden')
+      closeDropdown()
+
+      let parent = e.target.parentNode
+      if (parent.classList.contains('subnav') && shouldOpen) {
+        parent.classList.toggle('border-red-600')
+        parent.classList.toggle('border-solid')
+        parent.classList.toggle('border-b-4')
+        parent.querySelector('span>i').classList.toggle('transform')
+        parent.querySelector('span>i').classList.toggle('rotate-180')
+        parent.querySelector('div').classList.toggle('hidden')
+      }
+    })
+  })
+
+  window.onclick = function (e) {
+    if (!e.target.matches('.subnav>span')) {
+      closeDropdown()
+    }
+  }
+});
